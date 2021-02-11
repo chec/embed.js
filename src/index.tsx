0cea857a9186a8e479621372000e8f528a5b9ea5
Robbie Averill <robbie@chec.io>
2021-02-11 15:41:11 -0800

    Rebuild embed.js to use React and handle embeddable Rays (e.g. using Beam Checkout)

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Embed from './Embed';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Embed />
+  </React.StrictMode>,
+  document.getElementById('chec-embed')
+);
